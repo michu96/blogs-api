37176d5398a18896a7e1ace8f1b3bce5ecf2633d
Michal Trudzinski <mihu1701@gmail.com>
2021-03-14 14:26:54 +0100

    Create Blog model

---
+++ models/Blog.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 5,
+        maxLength: 100,
+    },
+    shortDescription: {
+        type: String,
+        trim: true,
+        maxLength: 250,
+        default: null,
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 5,
+        maxLength: 10000,
+    },
+    author: {
+        type: String,
+        trim: true,
+        minLength: 2,
+        maxLength: 100,
+        default: 'Anonymous',
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+    },
+    updatedAt: {
+        type: Date,
+        required: true,
+    },
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
