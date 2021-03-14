2031e9515cd39dafd5a83cbafed867a61c5407df
Michal Trudzinski <mihu1701@gmail.com>
2021-03-14 12:54:20 +0100

    Add listening + root and 404 middlewares

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("test");
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('<h1>Home page</h1>')
+})
+
+//Error 404 middleware
+app.use((req, res) => res.status(404).send('<h1>404 Not Found</h1>'))
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`))
