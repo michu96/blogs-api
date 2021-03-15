cb6d3fb7b2a172c0357cfdec9ddcaae04d256bc5
Michal Trudzinski <mihu1701@gmail.com>
2021-03-15 12:16:07 +0100

    Create API Blog routes

---
+++ routes/api/blog.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const blogController = require('../../controllers/blog-controller')
+
+const router = express.Router()
+
+router.get('/', blogController.read)
+router.get('/:id', blogController.readOne)
+router.post('/', blogController.insert)
+router.put('/:id', blogController.edit)
+router.delete('/:id', blogController.remove)
+
+module.exports = router
