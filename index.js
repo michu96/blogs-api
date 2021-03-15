cda36a76a55c72c9705b2b5845cd6d7e00bf08db
Michal Trudzinski <mihu1701@gmail.com>
2021-03-15 12:26:32 +0100

    Connect do database and make API work

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+require('dotenv').config()
 
 const app = express()
+
+mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+})
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('<h1>Home page</h1>')
 })
+
+app.use('/api', require('./routes/api/blog'))
 
 //Error 404 middleware
 app.use((req, res) => res.status(404).send('<h1>404 Not Found</h1>'))
