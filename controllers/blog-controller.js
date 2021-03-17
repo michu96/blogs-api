8c59a7e4938fd74ed387e6208cd4ed55ae9a15e1
Michal Trudzinski <mihu1701@gmail.com>
2021-03-17 15:16:19 +0100

    Sort and limit blogs, change default port to 5000

--- controllers/blog-controller.js
+++ controllers/blog-controller.js
@@ -1,100 +1,100 @@
 const Blog = require('../models/Blog')
 
 const blogController = {
-    read: async (req, res) => {
-        try {
-            const blogs = await Blog.find()
-            res.send(blogs)
-        } catch (err) {
-            res.status(500).send({
-                error: '500 Internal Server Error',
-            })
+  read: async (req, res) => {
+    try {
+      const blogs = await Blog.find().sort({ createdAt: 'desc' }).limit(15)
+      res.send(blogs)
+    } catch (err) {
+      res.status(500).send({
+        error: '500 Internal Server Error',
+      })
+    }
+  },
+  readOne: async (req, res) => {
+    // Check if given id is ObjectId format
+    if (req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
+      try {
+        const blog = await Blog.findById(req.params.id)
+        if (blog === null) {
+          res.status(404).send({ error: '404 Not Found' })
+          return
         }
-    },
-    readOne: async (req, res) => {
-        // Check if given id is ObjectId format
-        if (req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
-            try {
-                const blog = await Blog.findById(req.params.id)
-                if (blog === null) {
-                    res.status(404).send({ error: '404 Not Found' })
-                    return
-                }
-                res.send(blog)
-            } catch (err) {
-                res.status(500).send({ error: '500 Internal Server Error' })
-            }
-        } else {
-            res.status(400).send({
-                error: 'Invalid format of ID. Expected ObjectId',
-            })
-        }
-    },
-    insert: async (req, res) => {
-        const now = Date.now()
-        const blog = new Blog({
-            title: req.body.title,
-            shortDescription: req.body.shortDescription,
-            description: req.body.description,
-            author: req.body.author,
-            createdAt: now,
+        res.send(blog)
+      } catch (err) {
+        res.status(500).send({ error: '500 Internal Server Error' })
+      }
+    } else {
+      res.status(400).send({
+        error: 'Invalid format of ID. Expected ObjectId',
+      })
+    }
+  },
+  insert: async (req, res) => {
+    const now = Date.now()
+    const blog = new Blog({
+      title: req.body.title,
+      shortDescription: req.body.shortDescription,
+      description: req.body.description,
+      author: req.body.author,
+      createdAt: now,
+      updatedAt: now,
+    })
+    try {
+      await blog.save()
+      res.send({ msg: 'Created new blog', blog })
+    } catch (err) {
+      res.status(400).send({ error: 'Something went wrong' })
+    }
+  },
+  edit: async (req, res) => {
+    // Check if given id is ObjectId format
+    if (req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
+      const now = Date.now()
+      try {
+        const blog = await Blog.findByIdAndUpdate(
+          req.params.id,
+          {
+            ...req.body,
             updatedAt: now,
+          },
+          { new: true, runValidators: true }
+        )
+        if (blog === null) {
+          res.status(404).send({ error: '404 Not Found' })
+          return
+        }
+        res.send({ msg: 'Blog edited successfully', blog })
+      } catch (err) {
+        res.status(400).send({
+          error: 'Something went wrong',
         })
-        try {
-            await blog.save()
-            res.send({ msg: 'Created new blog', blog })
-        } catch (err) {
-            res.status(400).send({ error: 'Something went wrong' })
+      }
+    } else {
+      res.status(400).send({
+        error: 'Invalid format of ID. Expected ObjectId',
+      })
+    }
+  },
+  remove: async (req, res) => {
+    // Check if given id is ObjectId format
+    if (req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
+      try {
+        const blog = await Blog.findByIdAndDelete(req.params.id)
+        if (blog === null) {
+          res.status(404).send({ error: '404 Not Found' })
+          return
         }
-    },
-    edit: async (req, res) => {
-        // Check if given id is ObjectId format
-        if (req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
-            const now = Date.now()
-            try {
-                const blog = await Blog.findByIdAndUpdate(
-                    req.params.id,
-                    {
-                        ...req.body,
-                        updatedAt: now,
-                    },
-                    { new: true, runValidators: true }
-                )
-                if (blog === null) {
-                    res.status(404).send({ error: '404 Not Found' })
-                    return
-                }
-                res.send({ msg: 'Blog edited successfully', blog })
-            } catch (err) {
-                res.status(400).send({
-                    error: 'Something went wrong',
-                })
-            }
-        } else {
-            res.status(400).send({
-                error: 'Invalid format of ID. Expected ObjectId',
-            })
-        }
-    },
-    remove: async (req, res) => {
-        // Check if given id is ObjectId format
-        if (req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
-            try {
-                const blog = await Blog.findByIdAndDelete(req.params.id)
-                if (blog === null) {
-                    res.status(404).send({ error: '404 Not Found' })
-                    return
-                }
-                res.send({ msg: 'Blog deleted successfully', blog })
-            } catch (err) {
-                res.status(400).send({ error: 'Something went wrong' })
-            }
-        } else {
-            res.status(400).send({
-                error: 'Invalid format of ID. Expected ObjectId',
-            })
-        }
-    },
+        res.send({ msg: 'Blog deleted successfully', blog })
+      } catch (err) {
+        res.status(400).send({ error: 'Something went wrong' })
+      }
+    } else {
+      res.status(400).send({
+        error: 'Invalid format of ID. Expected ObjectId',
+      })
+    }
+  },
 }
 
 module.exports = blogController
--- index.js
+++ index.js
@@ -7,16 +7,16 @@ require('dotenv').config()
 const app = express()
 
 mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
 })
 
 app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('<h1>Home page</h1>')
+  res.send('<h1>Home page</h1>')
 })
 
 app.use('/api', require('./routes/api/blog'))
@@ -24,5 +24,5 @@ app.use('/api', require('./routes/api/blog'))
 //Error 404 middleware
 app.use((req, res) => res.status(404).send('<h1>404 Not Found</h1>'))
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`))
